952590a32967781b1e0b0adc335fea4b2b120123
syamirulamri <55517036+syamirulamri@users.noreply.github.com>
2019-09-23 08:59:11 +0800

    Update app_logic.php

--- login/app_logic.php
+++ login/app_logic.php
@@ -4,7 +4,7 @@ session_start();
 $errors = [];
 $user_id = "";
 // connect to database
- $db = pg_connect("host=localhost dbname=db_intra user=postgres password=abc123");
+include('db.php');
 
 // LOG USER IN
 
@@ -14,10 +14,10 @@ $user_id = "";
   Send email to user to reset their password
 */
 if (isset($_POST['reset-password'])) {
-  $email = pg_escape_string($db, $_POST['email']);
+  $email = pg_escape_string($con, $_POST['email']);
   // ensure that the user exists on our system
   $query ="SELECT email FROM tbl_employee WHERE email='$email'";
-  $results = pg_query($db, $query);
+  $results = pg_query($con, $query);
 
   if (empty($email)) {
     array_push($errors, "Your email is required");
@@ -30,7 +30,7 @@ if (isset($_POST['reset-password'])) {
   if (count($errors) == 0) {
     // store token in the password-reset database table against the user's email
     $sql = "INSERT INTO password_resets(email, token) VALUES ('$email', '$token')";
-    $results = pg_query($db, $sql);
+    $results = pg_query($con, $sql);
 
     // Send email to user with the token in a link they can click on
     $to = $email;
@@ -45,8 +45,8 @@ if (isset($_POST['reset-password'])) {
 
 // ENTER A NEW PASSWORD
 if (isset($_POST['new_password'])) {
-  $new_pass = pg_escape_string($db, $_POST['new_pass']);
-  $new_pass_c = pg_escape_string($db, $_POST['new_pass_c']);
+  $new_pass = pg_escape_string($con, $_POST['new_pass']);
+  $new_pass_c = pg_escape_string($con, $_POST['new_pass_c']);
 
   // Grab to token that came from the email link
   $token = $_SESSION['token'];
@@ -55,13 +55,13 @@ if (isset($_POST['new_password'])) {
   if (count($errors) == 0) {
     // select email address of user from the password_reset table 
     $sql = "SELECT email FROM password_resets WHERE token='$token' LIMIT 1";
-    $results = pg_query($db, $sql);
+    $results = pg_query($con, $sql);
     $email = pg_fetch_assoc($results)['email'];
 
     if ($email) {
       $new_pass = md5($new_pass);
       $sql = "UPDATE tbl_employee SET password='$new_pass' WHERE email='$email'";
-      $results = pg_query($db, $sql);
+      $results = pg_query($con, $sql);
       header('location: index.php');
     }
   }
