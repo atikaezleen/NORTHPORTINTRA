7b509d7d42a4b5cab22395dd4c112a5f3346f758
syamirulamri <55517036+syamirulamri@users.noreply.github.com>
2019-09-23 11:03:53 +0800

    Update reservation_update.php
    query changed

--- websites/ISD/company/reservation/admin/reservation_update.php
+++ websites/ISD/company/reservation/admin/reservation_update.php
@@ -3,13 +3,13 @@ session_start();
 include('db.php');
 	
 	$id = $_POST['id'];
-	$last = $_POST['last'];
+	$staff = $_POST['r_staff'];
 	$first = $_POST['first'];
 	$address = $_POST['address'];
 	$contact = $_POST['contact'];
 	$mode = $_POST['mode'];
 
-	pg_query($con,"UPDATE reservation SET r_last='$last',r_first='$first',r_address='$address',r_contact='$contact',modeofpayment='$mode' where rid='$id'") or die(pg_last_error($con)); 
+	pg_query($con,"UPDATE reservation SET r_staff='$staff',r_first='$first',r_address='$address',r_contact='$contact',modeofpayment='$mode' where rid='$id'") or die(pg_last_error($con)); 
 	echo "<script>document.location='reservation.php'</script>";   
 	
 	
